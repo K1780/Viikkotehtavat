b2878ea7fea6b7b4449c5324baeb56c35da48ddb
jesse <jesse.luotio@hotmail.com>
2017-01-17 23:28:00 +0200

    Viikkotehtavat

--- tehtava 2/Program.cs
+++ tehtava 2/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Jesse Luotio
+
 namespace tehtava_2
 {
     class Program
--- tehtava 5/Program.cs
+++ tehtava 5/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Jesse Luotio
+
 namespace tehtava_5
 {
     class Program
